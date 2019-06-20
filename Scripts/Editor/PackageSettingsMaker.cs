359cee568b4a0e02c6d8d7d566add30836e7212d
KentaYanase <yana.ken.physics@gmail.com>
2019-06-21 00:39:47 +0900

    SettingMakerの修正

--- Scripts/Editor/PackageSettingsMaker.cs
+++ Scripts/Editor/PackageSettingsMaker.cs
@@ -11,37 +11,38 @@ namespace ICKX
     {
         static PackageSettingsMaker()
         {
-            if (Application.isPlaying) return;
+			EditorApplication.delayCall += () =>
+			{
+				if (Application.isPlaying) return;
 
-            Dictionary<string, string> classNameFilePathTable = new Dictionary<string, string>();
+				Dictionary<string, string> classNameFilePathTable = new Dictionary<string, string>();
 
-            string[] guids = AssetDatabase.FindAssets("AutoCreatePackageSettings");
-            foreach (var guid in guids)
-            {
-                string path = AssetDatabase.GUIDToAssetPath(guid);
-                if (string.IsNullOrEmpty(path)) continue;
+				string[] guids = AssetDatabase.FindAssets("AutoCreatePackageSettings");
+				foreach (var guid in guids)
+				{
+					string path = AssetDatabase.GUIDToAssetPath(guid);
+					if (string.IsNullOrEmpty(path)) continue;
 
-                TextAsset text = AssetDatabase.LoadMainAssetAtPath(path) as TextAsset;
-                if (text == null) continue;
+					TextAsset text = AssetDatabase.LoadMainAssetAtPath(path) as TextAsset;
+					if (text == null) continue;
+					if (System.IO.Path.GetExtension(path) == ".cs") continue;
 
-                foreach (var line in text.text.Split('\n'))
-                {
-                    string[] data = line.Split(':');
-                    if (data.Length != 2) continue;
-                    string typeName = data[0];
-                    if (!classNameFilePathTable.ContainsKey(data[0]))
-                    {
-                        classNameFilePathTable[data[0]] = data[1];
-                    }
-                    else
-                    {
-                        Debug.LogError("同名のClassが設定されています : " + data[0]);
-                    }
-                }
-            }
+					foreach (var line in text.text.Split('\n'))
+					{
+						string[] data = line.Split(':');
+						if (data.Length != 2) continue;
+						string typeName = data[0];
+						if (!classNameFilePathTable.ContainsKey(data[0]))
+						{
+							classNameFilePathTable[data[0]] = data[1];
+						}
+						else
+						{
+							Debug.LogError("同名のClassが設定されています : " + data[0]);
+						}
+					}
+				}
 
-            EditorApplication.delayCall += () =>
-            {
                 AssetDatabase.StartAssetEditing();
                 foreach (Assembly assembly in System.AppDomain.CurrentDomain.GetAssemblies())
                 {
@@ -76,6 +77,11 @@ namespace ICKX
         private static void CreatePackageSettings(System.Type type, string filePath)
         {
             if (!type.IsSubclassOf(typeof(ScriptableObject))) return;
+
+            if(!AssetDatabase.IsValidFolder("Assets/Resources"))
+            {
+                AssetDatabase.CreateFolder("Assets", "Resources");
+            }
 
             if (string.IsNullOrEmpty(filePath))
             {
